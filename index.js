c9a693f5e5561354525e451f7be02211b674cc31
crevils <Kumar.kislay051@gmail.com>
2024-01-06 10:37:56 +0530

    editing cors policy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
   credentials: true
 };
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser(process.env.COOKIE_SIGNATURE))
